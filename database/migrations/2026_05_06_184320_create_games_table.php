dfda2c48bc44a4a5254dc6b8d7307911a2572dca
JuanFran <97101963+Xebir@users.noreply.github.com>
2026-05-10 13:50:34 +0200

    Cambios a migraciones

--- database/migrations/2026_05_06_184320_create_games_table.php
+++ database/migrations/2026_05_06_184320_create_games_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('games', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->integer('floor')->default(1);
             $table->timestamps();
+        });
+
+        Schema::table('games', function (Blueprint $table) {
+            $table->index('user_id');
         });
     }
 
--- database/migrations/2026_05_06_184334_create_characters_table.php
+++ database/migrations/2026_05_06_184334_create_characters_table.php
@@ -13,7 +13,28 @@ return new class extends Migration
     {
         Schema::create('characters', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('game_id')->constrained()->onDelete('cascade');
+            $table->foreignId('pasive_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('name');
+            $table->string('imagen')->nullable();
+            $table->integer('hp');
+            $table->integer('max_hp');
+            $table->integer('physical_attack');
+            $table->integer('special_attack');
+            $table->integer('physical_defense');
+            $table->integer('special_defense');
+            $table->integer('speed');
+            $table->integer('exp')->default(0);
+            $table->integer('level')->default(1);
+            $table->boolean('recruited')->default(false);
+            $table->boolean('alive')->default(true);
             $table->timestamps();
+        });
+
+        Schema::table('characters', function (Blueprint $table) {
+            $table->index('game_id');
+            $table->index('pasive_id');
+            $table->index('alive');
         });
     }
 
